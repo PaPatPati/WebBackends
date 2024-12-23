5b6d8bec909559a1237b06d55b3e83c62d7dfd38
Patrycja <114762725+PaPatPati@users.noreply.github.com>
2024-12-23 18:39:55 +0100

    Initial commit

---
+++ Models/Bid.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBackendsBidding.Models
+{
+    public class Bid
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+
+        [Required] //Wenn User gelöscht, soll das Bid nicht mehr vorhanden sein
+        public string? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+
+        public int? ListedArticleId { get; set; }
+        [ForeignKey("ListedArticleId")]
+        public ListedArticle? ListedArtikle { get; set; }
+    }
+}
---
+++ Models/ListedArticle.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBackendsBidding.Models
+{
+    public class ListedArticle
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public double Price { get; set; }
+        public bool IsSold { get; set; }
+        public string PathToImage { get; set; }
+
+        [Required] //Wenn User gelöscht soll der Artikel nicht mehr gelistet sein
+        public string? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+        public List<Bid?> Bids { get; set; }
+
+
+    }
+}
